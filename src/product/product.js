45ca7f8f0ac0b37f36240ae420132ed45acc023e
snbfan <snb.fan@gmail.com>
2024-07-29 12:03:33 +0200

    reset back the color param

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,9 @@
 class Product {
-    constructor(id, type, name, version, color) {
+    constructor(id, type, name, version) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-        this.color = color;
     }
 }
 
